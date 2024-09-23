6e884b316351808acb4f3c87bc9fa4f81b322faf
palicz <hdtoki@gmail.com>
2024-09-24 01:17:34 +0200

    chore: Added functionality to display 10 wordpairs in Game component and updated page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
 import MainMenu from './components/MainMenu';
 import Game from './components/Game';
+import wordData from './words.json';
+
+type WordPair = {
+  hungarian: string;
+  english: string;
+};
+
+// Load word list from JSON file
+const wordList: WordPair[] = wordData.wordList.slice(0, 10);
 
 const App: React.FC = () => {
-  const [showGame, setShowGame] = useState(false);
+  const [showGame, setShowGame] = useState(false); // State to toggle the Game component
 
+  // Function to start the game
   const handleStartGame = () => {
-    setShowGame(true);
+    setShowGame(true); // Set the state to true to show the Game component
   };
 
   return (
     <div>
+      {/* Render the MainMenu component if showGame is false */}
       {!showGame && <MainMenu onStartGame={handleStartGame} />}
-      {showGame && <Game />}
+      {/* Render the Game component if showGame is true */}
+      {showGame && <Game words={wordList} />}
     </div>
   );
 };
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import './Game.css';
 
-const Game: React.FC = ({}) => {
+// Define types for better code readability and type safety
+interface WordPair {
+  hungarian: string;
+  english: string;
+}
+
+interface GameProps {
+  words: WordPair[];
+}
+
+// Main Game component
+const Game: React.FC<GameProps> = ({words}) => {
+
   return (
-    <div>
-      <h1>Game</h1>
+    <div className="game-container">
+      <div className="columns">
+        <div className="column">
+          {words.map(word => (
+            <div key={word.hungarian} className="word">
+              {word.hungarian}
+            </div>
+          ))}
+        </div>
+        <div className="column">
+          {words.map(word => (
+            <div key={word.english} className="word">
+              {word.english}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Game;
